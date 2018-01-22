52465def6fdc36886984b2bdbdf019eb37f4c0c2
daniel@moberg.no <franken0795>
2018-01-22 17:45:52 +0100

    Picture mode skal funke no

--- app/src/main/java/com/example/danie/nameapp_v4/PictureActivity.java
+++ app/src/main/java/com/example/danie/nameapp_v4/PictureActivity.java
@@ -1,11 +1,17 @@
 package com.example.danie.nameapp_v4;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class PictureActivity extends AppCompatActivity {
@@ -15,6 +21,29 @@ public class PictureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
 
+        addPictures();
+    }
 
+    private void addPictures() {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout3);
+        final ArrayList<String> names = ((GlobalClass) this.getApplication()).getNames();
+
+        for (int i = 0; i < names.size(); i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(600, 600));
+
+            Uri uri = ((GlobalClass) this.getApplication()).getUri(names.get(i));
+
+            imageView.setImageURI(uri);
+
+            final int finalI = i;
+            imageView.setOnClickListener(view -> {
+                Intent intent = new Intent(PictureActivity.this, ShowPersonActivity.class);
+                intent.putExtra("name", names.get(finalI));
+                startActivity(intent);
+            });
+
+            layout.addView(imageView);
+        }
     }
 }
