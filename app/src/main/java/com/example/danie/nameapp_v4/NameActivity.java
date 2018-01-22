7ff6f927fe0154eb526a3dbca6d1037b8f9e7428
ahaggi <a.haggi@yahoo.com>
2018-01-22 16:26:46 +0100

    abc

--- app/src/main/java/com/example/danie/nameapp_v4/NameActivity.java
+++ app/src/main/java/com/example/danie/nameapp_v4/NameActivity.java
@@ -2,6 +2,10 @@ package com.example.danie.nameapp_v4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NameActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class NameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
+
+
+        ArrayList<String> names = new ArrayList<>();
+        for (String n:new String[]{"name1","name2","name3"}) {
+            names.add(n);
+        }
+
+
+        ArrayAdapter adapter = new ArrayAdapter<String>( this, R.layout.activity_name , names);
+
+        ListView namesList = (ListView) findViewById(R.id.namesList);
+        namesList.setAdapter(adapter);
+
+        namesList.setOnItemClickListener(
+                
+        );
+
     }
 }
